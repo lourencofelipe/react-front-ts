0c45696a288c307a3294c6d9a183cd6ddadea891
lourencofelipe <felipe.pucsc@gmail.com>
2022-03-11 21:01:22 -0300

    build: Adding styled components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com/' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com/'
+            crossOrigin='true'
+          />
+            <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
